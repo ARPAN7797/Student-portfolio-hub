12633b11a555a43d86f4caa900c692d19d516558
ARPAN7797 <arpanguchhait721629@gmail.com>
2025-09-09 08:47:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    // add other directories if needed
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./widgets/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: { extend: {} },
   plugins: [],
