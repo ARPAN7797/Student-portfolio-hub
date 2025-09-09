5cbfa636b4f1dada992b564da756ed6529b396be
ARPAN7797 <arpanguchhait721629@gmail.com>
2025-09-09 09:02:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/pages/**/*.{js,ts,jsx,tsx}",
-  "./src/components/**/*.{js,ts,jsx,tsx}",
-  // add others if needed
-],
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/widgets/**/*.{js,ts,jsx,tsx}",  // if you use this folder
+    "./src/app/**/*.{js,ts,jsx,tsx}",      // if you use this folder
+  ],
   theme: {
     extend: {},
   },
